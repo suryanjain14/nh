00d03830410242cf1f8c6aaded5d62e07b41a5be
suryan jain <suryanjain14@gmail.com>
2019-05-21 16:24:34 +0530

    still signals not working

--- users/signals.py
+++ users/signals.py
@@ -1,13 +1,13 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from django.dispatch import  receiver
-from.models import profile
+from django.dispatch import receiver
+from users.models import profile
 
 
 @receiver(post_save,sender=User)
 def create_profile(sender,instance,created,**kwargs):
     if created:
-        profile.object.create(user=instance)
+        profile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
