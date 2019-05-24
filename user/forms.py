ac84c925f6381c9ee23f6f428cc257a7776b5c84
Yashggoswami <41734202+Yashggoswami@users.noreply.github.com>
2019-05-24 19:38:46 +0530

    bhailog dekh lo

---
+++ user/forms.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+
+class user(UserCreationForm):
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control form-control needs-validation','placeholder':'Enter email'}))
+    first_name =forms.CharField(widget=forms.TextInput(attrs={'class':'form-inline form-control needs-validation ', 'placeholder':'First Name'}))
+    last_name= forms.CharField(widget=forms.TextInput(attrs={'class':' form-inline form-control needs-validation','placeholder':'Last Name'}))
+    username= forms.CharField(widget=forms.TextInput(attrs={'class':'form-group form-control needs-validation needs-validation','placeholder':'Last Name'}))
+    password1 = forms.CharField(label=("Password"),widget=forms.PasswordInput(attrs={'class':'form-group form-control needs-validation','placeholder':'Enter Password'}))
+    password2 = forms.CharField(label=("Password confirmation"),widget=forms.PasswordInput(attrs={'class':'form-group form-control needs-validation','placeholder':'Re-enter Password'}),)
+
+
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','username','email','password1','password2',]
+
+class login(LoginView):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-group form-control needs-validation needs-validation', 'placeholder': 'Last Name'}))
+    password = forms.CharField(label=("Password"), widget=forms.PasswordInput(
+        attrs={'class': 'form-group form-control needs-validation', 'placeholder': 'Enter Password'}))
+
+    class Meta:
+        model = User
+        fields =['username','password']
