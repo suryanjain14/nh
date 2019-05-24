cf24529238443dca5bde47f51aad334a41185ec3
Yashggoswami <41734202+Yashggoswami@users.noreply.github.com>
2019-05-24 19:38:46 +0530

    bhailog dekh lo

---
+++ user/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class UserConfig(AppConfig):
+    name = 'user'
+    def ready(self):
+        import user.signals
---
+++ user/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+
+class profile(models.Model):
+    user= models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(default='ppic.jpg',upload_to='ppic')
+    city = models.CharField(max_length=20,null=True,default='Indore')
+    country =models.CharField(max_length=20,null=True,default='India')
+    college=models.CharField(max_length=20,null=True,default='IIPS')
+    dob=models.DateField(max_length=20,null=True)
+
+    def __str__(self):
+
+        return f'{self.user.username } profile'
+
+
+
