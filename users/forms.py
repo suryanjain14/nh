7f5178179f6c90edfa3f3aac6efe623f6b6e91af
suryan jain <suryanjain14@gmail.com>
2019-06-03 18:50:16 +0530

    friends completed a minor bug in jinja templating is left

--- users/forms.py
+++ users/forms.py
@@ -5,12 +5,18 @@ from django.contrib.auth.views import LoginView
 from .models import profile
 
 class user(UserCreationForm):
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control form-control needs-validation','placeholder':'Enter email'}))
-    first_name =forms.CharField(widget=forms.TextInput(attrs={'class':'form-inline form-control needs-validation ', 'placeholder':'First Name'}))
-    last_name= forms.CharField(widget=forms.TextInput(attrs={'class':' form-inline form-control needs-validation','placeholder':'Last Name'}))
-    username= forms.CharField(widget=forms.TextInput(attrs={'class':'form-group form-control needs-validation needs-validation','placeholder':'Last Name'}))
-    password1 = forms.CharField(label=("Password"),widget=forms.PasswordInput(attrs={'class':'form-group form-control needs-validation','placeholder':'Enter Password'}))
-    password2 = forms.CharField(label=("Password confirmation"),widget=forms.PasswordInput(attrs={'class':'form-group form-control needs-validation','placeholder':'Re-enter Password'}),)
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={'class':'form-control form-control needs-validation','placeholder':'Enter email'}))
+    first_name =forms.CharField(widget=forms.TextInput(
+        attrs={'class':'form-inline form-control needs-validation ', 'placeholder':'First Name'}))
+    last_name= forms.CharField(widget=forms.TextInput(
+        attrs={'class':' form-inline form-control needs-validation','placeholder':'Last Name'}))
+    username= forms.CharField(widget=forms.TextInput(
+        attrs={'class':'form-group form-control needs-validation needs-validation','placeholder':'Last Name'}))
+    password1 = forms.CharField(label=("Password"),widget=forms.PasswordInput(
+        attrs={'class':'form-group form-control needs-validation','placeholder':'Enter Password'}))
+    password2 = forms.CharField(label=("Password confirmation"),widget=forms.PasswordInput(
+        attrs={'class':'form-group form-control needs-validation','placeholder':'Re-enter Password'}),)
 
 
     class Meta:
@@ -27,6 +33,7 @@ class login(LoginView):
         model = User
         fields =['username','password']
 
+
 class userupdateform(forms.ModelForm):
 
     email = forms.EmailField(widget=forms.TextInput(
@@ -39,7 +46,19 @@ class userupdateform(forms.ModelForm):
         model = User
         fields = ['first_name', 'last_name','email']
 
+
 class profileupdateform(forms.ModelForm):
+    image = forms.ImageField(widget=forms.FileInput(
+    #    attrs={'class' : "btn btn-secondary", 'type': "button", 'placeholder': 'Choose image' }
+    ))
+    city = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control form-control needs-validation'}))
+    country = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control form-control needs-validation'}))
+    college = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control form-control needs-validation'}))
+    dob = forms.DateField(widget=forms.DateInput(
+        attrs={'class': 'form-control form-control needs-validation'}))
     class Meta:
         model = profile
         fields = ['image','city','country','college','dob']
