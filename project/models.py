7851556141fb801b00f509a44dcd99c62bef2457
suryan jain <suryanjain14@gmail.com>
2019-07-01 15:43:56 +0530

    er digram project and group half completed

--- project/models.py
+++ project/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Project(models.Model):
+    # this model defines the project details
     name = models.TextField(max_length=20)
     data = models .TextField()
     level = models.TextField(max_length=20)
@@ -15,28 +16,28 @@ class Project(models.Model):
 
 
 class Userpro(models.Model):
+    # this model define just many to many relationship of user with project
+
     user = models.ManyToManyField(User)
     current_project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
 
 class Userprodetal(models.Model):
+    # this model define the details of prject given to a particular user like how much project user has completed
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    completion = models.BooleanField(default=False)
+    completion = models.IntegerField(default=0)
 
+
+#now we have to buid the logic for these models like that of friends
 
 '''
     @classmethod
-    def start_project(cls, current_project, new_friend):
+    def start_project(cls, current_project, user):
         project, created = cls.objects.get_or_create(
             current_project=current_project
         )
-        Userpro.users.add(new_friend)
+        Userpro.users.add(user)
 
-    @classmethod
-    def finish_project(cls, current_user, new_friend):
-        project, created = cls.objects.get_or_create(
-            current_user=current_user
-        )
-        project.users.remove(new_friend)
+    
 '''
