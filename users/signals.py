9375b841ee376b4723e6df718c1ff87b5d5ffde5
suryan jain <suryanjain14@gmail.com>
2019-07-02 19:33:46 +0530

    er digram project and group half completed

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+import django.dispatch
 from .models import profile
 
 
