335d479667a2c90ab37f414184b4d445d1b6d717
suryan jain <suryanjain14@gmail.com>
2019-07-03 22:28:22 +0530

    er digram project and group half completed

--- project/models.py
+++ project/models.py
@@ -6,22 +6,29 @@ from django.contrib.auth.models import User
 
 class Project(models.Model):
     # this model defines the project details
-    name = models.TextField(max_length=20)
+    name = models.TextField(max_length=20, unique=True)
     data = models .TextField()
     level = models.TextField(max_length=20)
-    tag = models.TextField(max_length=20, null=True)
     #timestamp = models.PositiveSmallIntegerField(default=1, max_length='2')
 
     def __str__(self):
         return self.name
 
 
+class ProTags(models.Model):
+    # this model store the tags of projects
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=16)
+
+    def __str__(self):
+        return f'{self.project}'
+
 class Userpro(models.Model):
-    # this model define just many to many relationship of user with project and details of that project for the user
+    # this model define just many to many relationship of user
 
     project = models.ManyToManyField(Project)
     current_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    completion = models.IntegerField(default=0)
+
 
     # now we have to buid the logic for these models like that of friends
     @classmethod
@@ -36,9 +43,12 @@ class Userpro(models.Model):
 
 
 class Pro_stat(models.Model):
+    # ye model complete karne ke liye signals ki zaroorat pade gi this models stores whtat is the conditions of user for a project
+    # model complete hai bas signal ke through is mai parameters bhejne hai ki jai se hi koi project start karthai
+    # to us  user  pk aur project ki pk is mai save ho jye
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    completion = models.IntegerField(default=0)
+    completion = models.IntegerField(default=25)
 
     class Meta:
         unique_together = (("user", "project"),)
@@ -53,5 +63,4 @@ class Pro_stat(models.Model):
 bhai log ek signal banana hai jo ki new project start karne par matlab 
 userpro mai save() ya many to many field ke change hone par active ho
 aur Pro_stat ke models mai user aur project ke column mai data dale
-
 '''
--- project/views.py
+++ project/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def project(request):
+    return render(request, 'project/project.html')
