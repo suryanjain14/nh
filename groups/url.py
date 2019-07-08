bdba9a53b81e1ec0cd308aca332d69fb4dd0127f
suryan jain <suryanjain14@gmail.com>
2019-07-08 15:43:38 +0530

    groups 50 percent completed

---
+++ groups/url.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+
+    path('/groupdb/<pk>/', views.groupdb, name='groupdb'),
+    path('/gp/<pk>', views.groupprofile, name='groupprofile')
+]
