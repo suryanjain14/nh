a5d1b03686c4ef039bed9575b04a65cd3fb70184
Yashggoswami <41734202+Yashggoswami@users.noreply.github.com>
2019-07-09 21:12:49 +0530

    contact us

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 def cont(request):
-    return render(request,"contact/index.html",{})
+    u_form=User.objects.get(username=request.user)
+    return render(request,"contact/index.html",{'u_form':u_form})
