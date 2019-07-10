92d072ba46805cbdcef2c2c4ae43860ae3aae864
suryan jain <suryanjain14@gmail.com>
2019-07-10 11:27:40 +0530

    bungs in  group forms

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+
+
 def cont(request):
-    u_form=User.objects.get(username=request.user)
-    return render(request,"contact/index.html",{'u_form':u_form})
+    try:
+        u_form = User.objects.get(username=request.user)
+
+    except:
+        u_form = None
+
+    return render(request, "contact/index.html", {'u_form': u_form})
