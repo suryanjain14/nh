6bfe9ad396f62206bcdbd64e9047a4daca302a3f
suryan jain <suryanjain14@gmail.com>
2019-07-12 12:47:41 +0530

    group newdb test1

--- groups/admin.py
+++ groups/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from .models import Group
+from .models import Group, GroupAdmin, Groupprojects, Groupuser
 
 
 admin.site.register(Group)
+admin.site.register(Groupuser)
+admin.site.register(Groupprojects)
+admin.site.register(GroupAdmin)
 '''
 from .models import Usrgroup, Groupdetails, Progroup
 # Register your models here.
