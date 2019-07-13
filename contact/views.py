85ab9fd8f53c7a23fd4c2d8bf73f0972e632c764
Yashggoswami <41734202+Yashggoswami@users.noreply.github.com>
2019-07-13 19:54:14 +0530

    contact message

--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
-
+from django.core.mail import send_mail
+from nh import settings
+from django.contrib import messages
 
 def cont(request):
     try:
         u_form = User.objects.get(username=request.user)
 
+
     except:
         u_form = None
+
+    if request.method == "POST":
+
+
+            subject = 'Thankyou for registering on NerdHerd!!'
+            message = 'this is message'
+            from_email = [u_form.email]
+            to_list = ['nerdherdindore@gmail.com']
+            send_mail(subject, message, from_email, to_list, fail_silently=True)
+            return redirect('home')
 
     return render(request, "contact/index.html", {'u_form': u_form})
