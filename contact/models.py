6ad7c5a6f97570ae96f12207dd52e39bed91650d
Yashggoswami <41734202+Yashggoswami@users.noreply.github.com>
2019-07-16 13:59:45 +0530

    contact message

--- contact/models.py
+++ contact/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+class msg(models.Model):
+    message=models.TextField()
+
+class msguser(models.Model):
+    msgusername=models.CharField(max_length=100,blank=True)
+    email=models.EmailField(max_length=100)
