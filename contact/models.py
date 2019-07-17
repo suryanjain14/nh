a37db07076dc5384adafceabd99ff35ac6b30b6c
bhalla08 <bhalla.varun801@gmail.com>
2019-07-17 14:06:44 +0530

    html file of privacy policy and terms of conndition uploaded

--- contact/models.py
+++ contact/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class msg (models.Model):
     message=models.TextField()
 
-class msguser (models.Model):
-    msgusername = models.CharField (max_length=100,blank=True)
+class msguser(models.Model):
+    msgusername = models.CharField(max_length=100,blank=True)
     email=models.EmailField (max_length=100)
