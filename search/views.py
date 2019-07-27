592a9d707c04778a200fd5ebc2e836d64cf38bd6
Anmol Pal <anmolpal1999@gmail.com>
2019-07-27 20:00:08 +0530

    nh

--- search/views.py
+++ search/views.py
@@ -12,11 +12,3 @@ class SearchProjectView(ListView):
         context['query'] = query
         return context
 
-    def get_queryset(self, *args, **kwargs):
-        request = self.request
-        method_dict = request.GET
-        query = method_dict.get('q', None )
-
-        if query is not None:
-            return Project.objects.filter(name__icontains=query)
-        return Project.objects.featured()
