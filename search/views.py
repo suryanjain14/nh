acd776ff992284e0e88dfa02f0b5f8b2e10f2cb2
Anmol Pal <anmolpal1999@gmail.com>
2019-07-27 19:13:30 +0530

    nh

---
+++ search/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import ListView
+from projects.models import Project
+# Create your views here.
+
+class SearchProjectView(ListView):
+
+    template_name = "search/view.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProjectView, self).get_context_data(*args, **kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        return context
