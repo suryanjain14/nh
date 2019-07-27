9f67db6766367de069ca30e0ae71ee30561daafb
Anmol Pal <anmolpal1999@gmail.com>
2019-07-27 20:34:06 +0530

    nh

--- project/views.py
+++ project/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Project,Userpro,project1
 
 # Create your views here.
