2256f0a75f4ad8828dd92026e1d66d6c11825c9e
Suryan Jain <suryanjain14@gmail.com>
2019-08-06 15:09:20 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:

--- resources/views.py
+++ resources/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def resource(request):
-    return render(request, 'resource/resource.html')
+    return render(request, 'resource/index.html')
